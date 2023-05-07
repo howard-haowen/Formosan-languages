47686e4a4e2cef6a34082550535561671a4b387d
Haowen Jiang <howard.haowen@gmail.com>
2023-05-07 10:57:48 +0800

    Updated the author's contact page

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def main():
    + 💬 生活會話: [族語E樂園](http://web.klokah.tw)
    + 🧗 句型: [族語E樂園](http://web.klokah.tw)
    + 🔭 文法: [臺灣南島語言叢書](https://alilin.apc.gov.tw/tw/)
-- 詞典資料使用`PDFMiner` 將2019版的PDF檔轉成HTML，再用`BeautifulSoup`抓取句對，偶爾會出現族語跟華語對不上的情形。若發現錯誤，請[聯絡我📩](https://github.com/howard-haowen)。詞典中重複出現的句子已從資料集中刪除。
+- 詞典資料使用`PDFMiner` 將2019版的PDF檔轉成HTML，再用`BeautifulSoup`抓取句對，偶爾會出現族語跟華語對不上的情形。若發現錯誤，請[聯絡我📩](https://howard-haowen.rohan.tw/)。詞典中重複出現的句子已從資料集中刪除。
    + 📚 詞典: [原住民族語言線上詞典](https://e-dictionary.apc.gov.tw/Index.htm?fbclid=IwAR18XBJPj2xs7nhpPlIUZ-P3joQRGXx22rbVcUvp14ysQu6SdrWYvo7gWCc)
 
 ### 查詢方法
